e9783f14d53510af441e922205950c433a250c38
atze <ploeg@cwi.nl>
2013-11-29 13:55:18 +0100

    Still improving

--- Examples.hs
+++ Examples.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE TypeOperators,KindSignatures,TypeFamilies, DataKinds, AllowAmbiguousTypes #-}
 module Examples where
 import Prelude hiding ((.))
-import OpenRecVar
+import Records
 
 -- Examples from the paper "Extensible records with scoped labels" by Daan Leijen
 
@@ -22,7 +22,7 @@ name = Label :: Label "name"
 
 -- inferred type (cannot be written down because OpenRecVar.R is not exported):  origin :: Rec ('OpenRecVar.R '["x" ':= Integer, "y" ':= Integer])
 -- nice type:
-origin :: Rec ("x" := Integer :| "y" := Integer :| Empty)
+origin :: Rec ("x" :-> Double :| "y" :-> Double :| Empty)
 origin = x := 0 .| y := 0 .| empty
 -- { x=0.0, y=0.0 }
 
@@ -38,13 +38,14 @@ origin3 = z := 0 .| origin
 -- { x=0.0, y=0.0, z=0 }
 
 -- type is inferred!
-named ::  a -> Rec r -> Rec ("name" := a :| r)
+named ::  a -> Rec r -> Rec ("name" :-> a :| r)
 named s r = name := s .| r
-{-
+
 -- inferred type:
 -- distance
---  :: (Floating t, Get "y" m ~ t, Get "x" m ~ t) =>
---     OpenRec m -> Get "x" m
+--  :: (Floating t, (r :! "y") ~ t, (r :! "x") ~ t) =>
+--     Rec r -> r :! "x"
+-- which is the same as:
 
 distance p = sqrt $ p.x * p.x + p.y * p.y
 
@@ -52,11 +53,12 @@ test1 = distance (named "2d" origin) + distance origin3
 -- 0.0
 
 -- inferred type:
--- move :: (Num t1, Num t, Get "y" m ~ t1, Get "x" m ~ t) =>
---     OpenRec m -> Get "x" m -> Get "y" m -> OpenRec m
+-- move   :: (Num t1, Num t, r :! "y" ~ t1, r :! "x" ~ t) =>
+--            Rec r -> r :! "x" -> r :! "y" -> Rec r
 
-move p dx dy = update x (p.x + dx) $
-               update y (p.y + dy) p
+
+move p dx dy = x :<- p.x + dx .|
+               y :<- p.y + dy .| p
 
 test2 = move (named "foo" origin3) 10 10
 -- { name="foo", x=10.0, y=10.0, z=0 }
@@ -67,12 +69,19 @@ test2 = move (named "foo" origin3) 10 10
 --typerr2 = distance (x := 1 .| empty)
 
 freeext = x := 1 .| origin
+{-
+freeexterr = x :!= 1 .| origin
+Error:
+    Couldn't match type ‛'Records.LabelNotUnique "x"’
+                  with ‛'Records.LabelUnique "x"’
+    In the first argument of ‛(.|)’, namely ‛x :!= 1’
+-}
 
 selfst = (x := 2 .| x := True .| empty) . x
 -- 2
 selsnd = ((x := 2 .| x := True .| empty) .- x) . x
 -- True
--}
+
 {-
 -- variant tests
 testadam =  case inj x (1 :: Double) of
--- Records.hs
+++ Records.hs
@@ -40,21 +40,19 @@ module Records
 
  (
 
-             -- *  Labels and Label value pairs
+             -- *  Labels and label type pairs
              KnownSymbol(..),
              Label(..),
-             IsSingleton(..),
-             LV(..),
+             LT(..),
              -- * Rows
              Row, Empty , (:|), (:!), (:-),(:\),
              -- * Records
              Rec,   
              empty,
+             RecordOp(..),
+              (.|),
              (.!),
-             (.|),
-             (.!|),
              (.-),
-             update,
              Forall(..),
      ) 
 where
@@ -76,31 +74,25 @@ import Debug.Trace
 -- | A label 
 data Label (s :: Symbol) = Label
 
-class IsSingleton l where 
-  getVal :: l
-instance IsSingleton (Label l) where
-  getVal = Label
-
 instance KnownSymbol s => Show (Label s) where
   show = symbolVal
 
-infixr 6 :=
--- | A label-value pair
-data LV a b = a := b deriving Show
+infixr 6 :->
+-- | A label-type pair (data kind) 
+data LT b = Symbol :-> b
 
 {--------------------------------------------------------------------
   Row operations
 --------------------------------------------------------------------}
 
-newtype Row a = R [LV Symbol a] -- constructor not exported
+newtype Row a = R [LT a] -- constructor not exported
 
 type family Empty :: Row * where
   Empty = R '[]
 
 infixr 5 :|
--- | Inject a symbol (l) type (t) pair into a (sorted) list of such pairs
---   Afterwards, the resulting list is again sorted.
-type family (l :: LV Symbol *) :|  (r :: Row *) :: Row * where
+-- | Extend the row with a label-type pair
+type family (l :: LT *) :|  (r :: Row *) :: Row * where
   l :| (R x) = R (Inject l x)
 
 infixl 6 :!
@@ -112,39 +104,73 @@ type family (r :: Row *) :! (t :: Symbol) :: * where
 type family (r :: Row *) :- (s :: Symbol)  :: Row * where
   (R r) :- l = R (Remove l r)
 
-class (r :: Row *) :\ (l :: Symbol)
-instance (RLacks r l ~ True) => r :\ l
+type family (l :: Row *) :++  (r :: Row *)  :: Row * where
+  (R l) :++ (R r) = R (Merge l r)
+
+type family (l :: Row *) :+  (r :: Row *)  :: Row * where
+  (R l) :+ (R r) = R (Merge l r)
 
 -- | Does the row lack (i.e. it has not) the specified label?
-type family RLacks (r :: Row *)  (s :: Symbol) :: Bool where
-  RLacks (R r) l = Lacks l r
+class (r :: Row *) :\ (l :: Symbol)
+instance (Lacks l r ~ LabelUnique l) => (R r) :\ l
+
+-- | Are the two rows disjoint? (i.e. their set of labels is disjoint)
+class (l :: Row *) :\\ (r :: Row *)
+instance (Disjoint l r ~ IsDisjoint) => (R l) :\\ (R r)
 
 -- private things for row operations
 
 
-type family Inject (l :: LV Symbol *) (r :: [LV Symbol *]) where
-  Inject (l := t) '[] = (l := t ': '[])
-  Inject (l := t) (l' := t' ': x) = 
-      Ifte (l <=.? l')
-      (l := t   ': l' := t' ': x)
-      (l' := t' ': (Inject (l := t)  x))
+-- gives nicer error message than Bool
+data Unique = LabelUnique Symbol | LabelNotUnique Symbol
 
-type family Ifte (c :: Bool) (t :: [LV Symbol *]) (f :: [LV Symbol *])   where
+type family Inject (l :: LT *) (r :: [LT *]) where
+  Inject (l :-> t) '[] = (l :-> t ': '[])
+  Inject (l :-> t) (l' :-> t' ': x) = 
+      Ifte (l <=.? l')
+      (l :-> t   ': l' :-> t' ': x)
+      (l' :-> t' ': (Inject (l :-> t)  x))
+
+type family Ifte (c :: Bool) (t :: [LT *]) (f :: [LT *])   where
   Ifte True  t f = t
   Ifte False t f = f
 
-type family Get (l :: Symbol) (r :: [LV Symbol *]) where
-  Get l (l := t ': x) = t
-  Get l (l' := t ': x) = Get l x
+type family Get (l :: Symbol) (r :: [LT *]) where
+  Get l (l :-> t ': x) = t
+  Get l (l' :-> t ': x) = Get l x
 
-type family Remove (l :: Symbol) (r :: [LV Symbol *]) where
-  Remove l (l := t ': x) = x
-  Remove l (l' := t ': x) = l' := t ': Remove l x
+type family Remove (l :: Symbol) (r :: [LT *]) where
+  Remove l (l :-> t ': x) = x
+  Remove l (l' :-> t ': x) = l' :-> t ': Remove l x
 
-type family Lacks (l :: Symbol) (r :: [LV Symbol *]) where
-  Lacks l '[] = True
-  Lacks l (l := t ': x) = False
+type family Lacks (l :: Symbol) (r :: [LT *]) where
+  Lacks l '[] = LabelUnique l
+  Lacks l (l :-> t ': x) = LabelNotUnique l
 
+type family Merge (l :: [LT *]) (r :: [LT *]) where
+  Merge '[] r = r
+  Merge l '[] = l
+  Merge (hl :-> al ': tl) (hr :-> ar ': tr) = 
+      Ifte (hl <=.? hr)
+      (hl :-> al ': Merge tl (hr :-> ar ': tr))
+      (hr :-> ar ': Merge (hl :-> al ': tl) tr)
+
+-- gives nicer error message than Bool
+data DisjointErr = IsDisjoint | Duplicate Symbol
+
+type family IfteD (c :: Bool) (t :: DisjointErr) (f :: DisjointErr)   where
+  IfteD True  t f = t
+  IfteD False t f = f
+
+
+type family Disjoint (l :: [LT *]) (r :: [LT *]) where
+    Disjoint '[] r = IsDisjoint
+    Disjoint l '[] = IsDisjoint
+    Disjoint (l :-> al ': tl) (l :-> ar ': tr) = Duplicate l
+    Disjoint (hl :-> al ': tl) (hr :-> ar ': tr) = 
+      IfteD (hl <=.? hr)
+      (Disjoint tl (hr :-> ar ': tr))
+      (Disjoint (hl :-> al ': tl) tr)
 
 
 {--------------------------------------------------------------------
@@ -162,21 +188,26 @@ data Rec (r :: Row *) where
 empty :: Rec Empty
 empty = OR M.empty
 
+infix 5 :=
+infix 5 :!=
+infix 5 :<-
+data RecordOp in' out where
+   -- |  Record extension
+  (:=)  :: KnownSymbol l           => Label l -> a      -> RecordOp r (l :-> a :| r)
+  -- | Record extension, without shadowing
+  (:!=) :: (KnownSymbol l, r :\ l) => Label l -> a      -> RecordOp r (l :-> a :| r)
+  -- | Record update
+  (:<-) :: KnownSymbol l           => Label l -> r :! l -> RecordOp r r 
+
+-- | Apply an operation to a record.
 infixr 4 .|
-
--- | Record extension
-(.|) :: KnownSymbol l => LV (Label l) a -> Rec r -> Rec (l := a :| r)
-((show -> a) := b) .| (OR m) = OR $ M.insert a v m
-  where v = HideType b <| M.findWithDefault S.empty a m
-
-
-infix  4 .!|
--- | Record extension, without shadowing
-(.!|) :: (KnownSymbol l, r :\ l) => LV (Label l) a -> Rec r -> Rec (l := a :| r)
-(.!|) = (.|)
+(.|) :: RecordOp r r' -> Rec r -> Rec r'
+((show -> l) := a) .| (OR m)  = OR $ M.insert l v m where v = HideType a <| M.findWithDefault S.empty l m
+((show -> l) :!= a) .| (OR m) = OR $ M.insert l v m where v = HideType a <| M.findWithDefault S.empty l m
+((show -> l) :<- a) .| (OR m) = OR $ M.adjust f l m where f = S.update 0 (HideType a)  
 
 
-infix  9 .-
+infix  8 .-
 -- | Record selection
 (.!) :: KnownSymbol l => Rec r -> Label l -> r :! l
 (OR m) .! (show -> a) = x'
@@ -192,15 +223,17 @@ infix  9 .-
                EmptyL -> M.delete a m
                _      -> M.insert a t m
 
--- | Record update
-update :: KnownSymbol l =>  Label l -> r :! l -> Rec r -> Rec r
-update l v (OR m) = OR $ M.adjust f (show l) m
-  where f = S.update 0 (HideType v)  
+-- | Record merge (not commutative)
+(.++) :: Rec l -> Rec r -> Rec (l :++ r)
+(OR l) .++ (OR r) = OR $ M.unionWith (><) l r
+
+-- | Record disjoint union (commutative)
+(.+) :: (l :\\ r) => Rec l -> Rec r -> Rec (l :+ r)
+(OR l) .+ (OR r) = OR $ M.unionWith (error "Impossible") l r
 
 
-
-unsafeInjectFront :: KnownSymbol l => LV (Label l) a -> Rec (R r) -> Rec (R (l := a ': r))
-unsafeInjectFront ((show -> a) := b) (OR m) = OR $ M.insert a v m
+unsafeInjectFront :: KnownSymbol l => Label l -> a -> Rec (R r) -> Rec (R (l :-> a ': r))
+unsafeInjectFront (show -> a) b (OR m) = OR $ M.insert a v m
   where v = HideType b <| M.findWithDefault S.empty a m
 
 
@@ -210,9 +243,9 @@ class GetLabels (r :: Row *) where
 instance GetLabels (R '[]) where
   getLabels _ = []
 
-instance (KnownSymbol l, GetLabels (R t)) =>  GetLabels (R (l := a ': t)) where
+instance (KnownSymbol l, GetLabels (R t)) =>  GetLabels (R (l :-> a ': t)) where
   getLabels r = show l : getLabels (r .- l) where
-     l = getVal :: Label l
+     l = Label :: Label l
 
 class Forall (r :: Row *) (c :: * -> Constraint) where
  rinit     :: (forall a. c a => a) -> Rec r
@@ -224,18 +257,18 @@ instance Forall (R '[]) c where
   erase _ _ = []
   eraseZip _ _ _ = []
 
-instance (KnownSymbol l, Forall (R t) c, c a) => Forall (R ((l := a) ': t)) c where
-  rinit f = unsafeInjectFront (l := a) (initnxt f) where
-    l = getVal :: Label l
+instance (KnownSymbol l, Forall (R t) c, c a) => Forall (R (l :-> a ': t)) c where
+  rinit f = unsafeInjectFront l a (initnxt f) where
+    l = Label :: Label l
     a = (f :: a)
     initnxt = rinit :: (forall a. c a => a) -> Rec (R t)
-  erase ::  forall b. (forall a. c a => a -> b) -> Rec (R ((l := a) ': t)) -> [b]
+  erase ::  forall b. (forall a. c a => a -> b) -> Rec (R (l :-> a ': t)) -> [b]
   erase f r = f (r .! l) : erasenxt f (r .- l) where
-    l = getVal :: Label l
+    l = Label :: Label l
     erasenxt = erase :: (forall a. c a => a ->  b) -> Rec (R t) -> [b]
-  eraseZip ::  forall b. (forall a. c a => a -> a ->  b) -> Rec (R ((l := a) ': t)) ->  Rec (R ((l := a) ': t)) -> [b]
+  eraseZip ::  forall b. (forall a. c a => a -> a ->  b) -> Rec (R ((l :-> a) ': t)) ->  Rec (R ((l :-> a) ': t)) -> [b]
   eraseZip f x y = f (x .! l) (y .! l) : erasenxt f (x .- l) (y .- l) where
-    l = getVal :: Label l
+    l = Label :: Label l
     erasenxt = eraseZip :: (forall a. c a => a -> a -> b) -> Rec (R t) -> Rec (R t) -> [b]
   
 -- some standard type classes
